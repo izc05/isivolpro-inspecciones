d5c218a53cf4bc81b0a4fc24d549957b055af22e
izc05 <isiciozafracantos@gmail.com>
2026-05-09 10:21:30 +0200

    Fix GitHub Pages fallback

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/isivolpro-inspecciones/" : "/",
+  base: command === "build" ? "./" : "/",
   plugins: [react()],
 }));
