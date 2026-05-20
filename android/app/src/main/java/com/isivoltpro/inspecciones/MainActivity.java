b255c880c0368d09a4de21c121bd90bfdcabcdcf
izc05 <isiciozafracantos@gmail.com>
2026-05-20 18:08:12 +0200

    feat: preparar proyecto Android con Capacitor

---
+++ android/app/src/main/java/com/isivoltpro/inspecciones/MainActivity.java
@@ -0,0 +1,5 @@
+package com.isivoltpro.inspecciones;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,19 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.isivoltpro.inspecciones",
+  appName: "IsiVoltPro Inspecciones",
+  webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      backgroundColor: "#061F3E",
+      androidSplashResourceName: "splash",
+    },
+  },
+};
+
+export default config;
